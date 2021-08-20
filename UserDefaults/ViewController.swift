8bfeb6880bd5c6264b528b29e933bdb700527f6e
Sergey <piolersa85@mail.ru>
2021-08-20 17:36:30 +0200

    Initial Commit

---
+++ UserDefaults/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaults
+//
+//  Created by Sergey on 20.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
